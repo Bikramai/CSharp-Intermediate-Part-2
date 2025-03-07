c60a44338559b26539de49694a5be8a55e2c1abe
bikram <bikra@BIKRAM-LENOVO>
2025-03-07 14:13:57 -0500

    Methods

--- Program.cs
+++ Program.cs
@@ -1,35 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Classes
+namespace Methods
 {
-    public class Person
-    {
-        public string Name;
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}", to, Name);
-        }
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-
-            return person;
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var person = new Person();
-            var p = Person.Parse("John");
-            person.Name = "John";
-            person.Introduce("Bikram");
+            try
+            {
+                var num = int.Parse("abc");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Conversion failed");
+            }
 
+
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion failed");
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 4, 5, 6 }));
+        }
+
+        static void UsePoison(int x, int y)
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("An unexpected error occured.");
+            }
         }
     }
 }
