5751c35ca857c9e69c126a05ff61ea00df77da05
bikram <bikra@BIKRAM-LENOVO>
2025-03-13 13:23:39 -0400

    Inheritance -Access Modifiers - Need to redo

--- Program.cs
+++ Program.cs
@@ -1,23 +1,11 @@
-﻿using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
-using AccessModifiers;
-
+﻿
 namespace AccessModifiers
 {
-    public class GoldCustomer : Customer1
-    {
-        public void OfferVoucher()
-        {
-            var rating = this.CalculateRating(excludeOrders: true);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var customers = new Customer1();
+            var customers = new Customer();
         }
     }
 }
